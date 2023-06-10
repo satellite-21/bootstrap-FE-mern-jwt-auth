4031b45754b8b6cdd0d66308d17a8ea071bc0eef
satellite-21 <www.kartiksuccess832@gmail.com>
2023-06-10 18:21:40 +0530

    infinite-scroll

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,16 +1,3 @@
-// import UserPage from "./UserPage";
-// import React from "react";
-
-
-// const AdminPage = () => {
-//     return(
-//         <div>
-//             <UserPage />
-//         </div>
-//     )
-// }
-
-// export default AdminPage;
 import React, {useEffect, useState} from "react";
 import MovieBoxAdmin from "./movieBoxAdmin"
 import axios from "axios";  
@@ -23,45 +10,83 @@ const AdminPage = () => {
   const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
   const [description, setDescription] = useState("");
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
 
   useEffect(() => {
     fetchMovies();
-  }, []);
+  }, [page]);
 
   const fetchMovies = async () => {
     try{
-      const response = await axios.get("http://localhost:8080/api/movies");
-      setMovies(response.data);
+        setLoading(true);
+        const excludedMovies = movies.map(movie => movie._id);
+
+    //   const response = await axios.get("http://localhost:8080/api/movies");
+    const response = await axios.get(`http://localhost:8080/api/movies?page=${page}&search=${searchQuery}&excludedMovies=${excludedMovies}`);
+    //   setMovies(response.data);
+    setMovies((prevMovies) => [...prevMovies, ...response.data]);
+    setLoading(false);
     } catch (error) {
       console.log("Error fetching movies.", error);
       }
   };
 
 
-  const handleSearch = debounce(async () => {
-    try{
-      const response =  await axios.get(`http://localhost:8080/api/movies?search=${searchQuery}`);
-      setMovies(response.data);
-    } catch(error){
-      console.log("Error searching movies.", error);
-    }
-  }, 500);
+//   const handleSearch = debounce(() => {
+//     try{
+//       const response =  await axios.get(`http://localhost:8080/api/movies?search=${searchQuery}`);
+//       setMovies(response.data);
+//     } catch(error){
+//       console.log("Error searching movies.", error);
+//     }
+//   }, 500);
 
+const handleSearch = debounce(() => {
+    setPage(1);
+    setMovies([]);
+    fetchMovies();
+  }, 500);
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
     handleSearch();
   };
 
-  const handleDeleteMovie = async (movieId) => {
+//   const handleDeleteMovie = async (movieId) => {
+//     try {
+//       await axios.delete(`http://localhost:8080/api/movies/${movieId}`);
+//       fetchMovies();
+//     } catch (error) {
+//       console.log("Error deleting movie.", error);
+//     }
+//   };
+const handleDeleteMovie = async (movieId) => {
     try {
       await axios.delete(`http://localhost:8080/api/movies/${movieId}`);
-      fetchMovies();
+      setMovies((prevMovies) => prevMovies.filter(movie => movie._id !== movieId));
     } catch (error) {
       console.log("Error deleting movie.", error);
     }
   };
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight &&
+      !loading
+    ) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -125,7 +150,7 @@ const AdminPage = () => {
           <MovieBoxAdmin key={movie._id} id={movie._id} title={movie.Title} url={movie.URL} description={movie.Description} onDelete={handleDeleteMovie}/>
         ))}
       </div>
-
+      {loading && <p>Loading...</p>}
       
 
     </div>  
